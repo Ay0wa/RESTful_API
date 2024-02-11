edfa85dc50ead55087afc36300e4e14f927cd2ea
Ay0wa <Ay0wa>
2024-02-11 16:33:14 +0300

    a

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'restaurantdb', 
         'USER': 'root', 
-        'PASSWORD': 'Sk123321()', 
+        'PASSWORD': '#', 
         'HOST': '127.0.0.1', 
         'PORT': '3306', 
     } 
--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from api.views import index
 urlpatterns = [
+    path('', index, name="home"),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
